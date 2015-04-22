45bea40d05eba4e6cab2f2bb57ec9edebf63f804
tomyc <tomasz@cieplak.net>
2015-04-22 18:51:42 +0200

    Przykład 10

--- app/src/main/java/net/cieplak/firstapp_pinch/PinchMainActivity.java
+++ app/src/main/java/net/cieplak/firstapp_pinch/PinchMainActivity.java
@@ -71,7 +71,7 @@ public class PinchMainActivity extends ActionBarActivity {
 
         @Override
         public void onScaleEnd(ScaleGestureDetector detector) {
-            
+            scaleInfo.setText("");
         }
     }
 }
